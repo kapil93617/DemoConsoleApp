67179b7ccca30b3473227a5f777fcdf5b03c8c33
kapil93617 <51697236+kapil93617@users.noreply.github.com>
2019-06-18 16:57:01 +0530

    Add files via upload

---
+++ Sample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Demo
+    {
+        public void Display()
+        {
+            Console.WriteLine(hello);
+        }
+    }
+    class Sample
+    {
+        public void print()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+
+    class program
+    {
+        public static void Main(string [] args)
+        {
+            Sample sap = new Sample();
+            sap.print();
+        }
+    }
+
+
+}
